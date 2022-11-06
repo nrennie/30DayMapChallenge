9fff2c698fdb7d3d8f703e4aca867cefd4245676
nrennie <nrennie35@gmail.com>
2022-11-06 14:53:30 +0000

    day 8

--- 2022/scripts/08_osm.R
+++ 2022/scripts/08_osm.R
@@ -5,19 +5,23 @@ library(sf)
 #choose area
 bbx <- getbb("Newcastle, UK")
 
-#large roads
+# large roads
 roads <- bbx %>%
   opq() %>%
-  add_osm_feature(
-    key = "highway",
-    value = c(
-      "motorway",
-      "trunk",
-      "primary",
-      "secondary",
-      "tertiary"
-    )
-  ) %>%
+  add_osm_feature(key = "highway",
+                  value = c("motorway", "trunk", "primary",
+                            "secondary", "tertiary", "motorway_link",
+                            "trunk_link", "primary_link", "secondary_link",
+                            "tertiary_link")) %>%
+  osmdata_sf()
+
+# small roads
+streets <- bbx %>%
+  opq() %>%
+  add_osm_feature(key = "highway",
+                  value = c("residential", "living_street", "service",
+                            "unclassified", "pedestrian", "footway",
+                            "track", "path")) %>%
   osmdata_sf()
 
 # draw a circle
@@ -33,15 +37,21 @@ circle <- tibble(lat = centre["lat"], long = centre["long"]) %>%
 
 # intersection
 roads_new <- st_intersection(circle, roads$osm_lines)
+streets_new <- st_intersection(circle, streets$osm_lines)
 
 # plot
 ggplot() +
   geom_sf(data = roads_new,
-          size = 0.6) +
-  geom_sf(data = circle, fill = NA, size= 0.1) +
-  labs(title = "NEWCASTLE") +
+          size = 0.6, 
+          colour = alpha("black", 0.9)) +
+  geom_sf(data = streets_new,
+          size = 0.6, 
+          colour = alpha("black", 0.5)) +
+  labs(title = "NEWCASTLE",
+       caption = "N. Rennie | Data: {osmdata}") +
   theme_void() +
-    theme(plot.title = element_text(family = "mono", size = 24, hjust = 0.5, margin = margin(t = 10)))
+    theme(plot.title = element_text(family = "mono", size = 24, hjust = 0.5, margin = margin(t = 10)),
+          plot.caption = element_text(family = "mono", size = 10, hjust = 0.5, margin = margin(b = 10)))
 
 # save
 ggsave("2022/maps/day_08.png", height = 6, width = 6, bg = "white")
