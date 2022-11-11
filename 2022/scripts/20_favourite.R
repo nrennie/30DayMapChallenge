9ad641c7d9ff05b472535a5ac53c36f95abdb55e
nrennie <nrennie35@gmail.com>
2022-11-11 19:07:50 +0000

    day 20

--- 2022/scripts/20_favourite.R
+++ 2022/scripts/20_favourite.R
@@ -1,66 +1,37 @@
 library(tidyverse)
-library(ggmap)
-library(osmdata)
+library(sf)
 library(rcartocolor)
-library(rStrava)
-library(gganimate)
+library(showtext)
 
-# data wrangling
-strava_data %>% 
-  as_tibble() %>% 
-  select(-id)
+# Load fonts
+font_add_google("Barrio", "barrio")
+font_add_google("Ubuntu", "ubuntu")
+showtext_auto()
 
-# Define geographic area
-getbb("Newcastle, UK")
-bb <- matrix(c(-1.78, -1.34, 54.8, 55.1), 
-             ncol = 2, 
-             nrow = 2,
-             byrow = TRUE,
-             dimnames = list(c("x", "y"), c("min", "max")))
+#Data from https://www.data.gov.uk/dataset/6edd3b1b-16d8-477d-affd-702dd2f13936/geomorphology-of-the-cairngorm-mountains
+cairngorm_poly <- sf::st_read("2022/data/cairngorm/CAIRNGORM_GEOMORPH_poly.shp") 
 
-# Get a background map
-bg_map <- get_map(bb,
-                  source = "stamen",
-                  maptype = "toner-hybrid", 
-                  color = "bw")
-ggmap(bg_map)
-
-# plot using geom_point()
-g <- ggmap(bg_map) +
-  geom_point(data = strava_data,
-             inherit.aes = FALSE,
-             aes(colour = altitude, 
-                 x = lng, 
-                 y = lat), 
-             size = 1) + 
-  scale_colour_carto_c(name = "Altitude (m)", palette = "SunsetDark") +
-  labs(title = "Great North Run 2022",
-       caption = "N. Rennie") +
+# map
+ggplot() +
+  geom_sf(data = cairngorm_poly,
+          aes(fill = LANDFORM_A),
+          colour = "transparent") +
+  scale_fill_carto_d(palette = "Antique",
+                     breaks = c("Landforms of glacial and glaciofluvial deposition",
+                                "Landforms of glacial erosion",
+                                "Postglacial and contemporary landforms and processes",
+                                "Postglacial active alluvial fan surface",
+                                "Relict periglacial landforms",
+                                "Other"
+                                )) +
+  labs(title = "Cairngorms") +
+  guides(fill = guide_legend(ncol = 2)) +
   theme_void() +
-  theme(legend.position = c(0.2, 0.2), 
-        axis.title = element_blank(), 
-        legend.title = element_text(face = "bold", hjust = 0.5, colour = "white"),
-        legend.text = element_text(colour = "white"),
-        legend.margin = margin(10, 10, 10, 10),
-        legend.background = element_rect(fill = alpha("black", 0.5)),
-        plot.title = element_text(colour = "#dc3977",
-                                  size = 16,
-                                  hjust = 0.5,
-                                  margin = margin(b = 10)), 
-        plot.caption = element_text(colour = "#dc3977",
-                                    size = 10, 
-                                    hjust = 0.5,
-                                    margin = margin(t = 10)), 
-        plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), unit = "cm"))
-g
+  theme(legend.position = "bottom",
+        plot.margin = margin(10, 10, 10, 10),
+        legend.title = element_blank(),
+        legend.text = element_text(family = "ubuntu", lineheight = 0.5, size = 20),
+        plot.title = element_text(hjust = 0.5, size = 100, family = "barrio", margin = margin(b = 30)))
 
 # save
-ggsave("2022/maps/day_20.png", height = 6, width = 6, bg = "white")
-
-# animate 
-g = g + 
-  transition_time(time = time) +
-  shadow_mark()
-g
-animate(g, height = 576, width = 576)
-anim_save("2022/maps/day_20.gif")
+ggsave("2022/maps/day_20.png", height = 6, width = 6, bg = "grey70")
