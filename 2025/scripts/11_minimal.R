98886fd6bc927fc353b5604ee01d804927bc68f5
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-11-03 18:34:02 +0000

    tidy up day 11

--- 2025/scripts/11_minimal.R
+++ 2025/scripts/11_minimal.R
@@ -1,7 +1,4 @@
 
-source("2025/scripts/setup.R")
-
-
 # Packages ----------------------------------------------------------------
 
 library(gapminder)
@@ -10,6 +7,9 @@ library(tidyverse)
 library(ggforce)
 library(camcorder)
 library(glue)
+library(nrBrand)
+library(ggtext)
+library(showtext)
 
 
 # Start recording ---------------------------------------------------------
@@ -17,7 +17,7 @@ library(glue)
 gg_record(
   device = "png",
   width = 6,
-  height = 5.5,
+  height = 5,
   units = "in",
   dpi = 300
 )
@@ -25,24 +25,34 @@ gg_record(
 
 # Data --------------------------------------------------------------------
 
-plot_data <- gapminder |> 
-  filter(year == 2007) |> 
+plot_data <- gapminder |>
+  filter(year == 2007) |>
   left_join(
-    country_codes, by = "country"
-  ) |> 
+    country_codes,
+    by = "country"
+  ) |>
   select(iso_alpha, pop)
+
+
+# Fonts -------------------------------------------------------------------
+
+font_add_google("Ubuntu")
+showtext_auto()
+showtext_opts(dpi = 300)
 
 
 # Text --------------------------------------------------------------------
 
-social <- nrBrand::social_caption(
-  bg_colour = col_palette[5],
-  icon_colour = col_palette[3],
-  font_colour = col_palette[3],
+social <- social_caption(
+  bg_colour = "#0C2431",
+  icon_colour = "#FFFFFF",
+  font_colour = "#FFFFFF",
   font_family = "Ubuntu",
-  mastodon = NA
+  mastodon = NA,
+  bluesky = NA,
+  linkedin = NA
 )
-cap <- glue("<span style='font-size: 24pt'>**The World's Population**</span><br><br>**Data**: Gapminder<br>**Graphic**: {social}")
+cap <- glue("<span style='font-size: 24pt'>**Where are all the people?**</span><br><br>**Data** : Gapminder | **Graphic**: {social}")
 
 
 # Plot --------------------------------------------------------------------
@@ -53,38 +63,39 @@ p <- ggplot(
   geom_circle(
     mapping = aes(x0 = 1, y0 = 1, r = sqrt(pop)),
     colour = "transparent",
-    fill = col_palette[3]
+    fill = "#FFFFFF"
   ) +
-  facet_geo(~iso_alpha, 
-            grid = "world_countries_grid1",
-            label="code_alpha3") +
+  facet_geo(~iso_alpha,
+    grid = "world_countries_grid1",
+    label = "code_alpha3"
+  ) +
   labs(caption = cap) +
   coord_fixed(expand = FALSE) +
   theme_void() +
   theme(
     plot.background = element_rect(
-      fill = col_palette[5], colour = col_palette[5]
+      fill = "#0C2431", colour = "#0C2431"
     ),
     strip.text = element_blank(),
     legend.position = "none",
     panel.spacing = unit(-0.5, "lines"),
-    plot.margin = margin(10, 10, 10, 10),
-    plot.caption = ggtext::element_textbox_simple(
-      margin = ggplot2::margin(b = 0, t = -25),
+    plot.margin = margin(10, 10, 15, 10),
+    plot.caption = element_textbox_simple(
+      margin = margin(b = 0, t = -25),
       hjust = 0,
       halign = 0,
-      colour = col_palette[3],
+      colour = "#FFFFFF",
       family = "Ubuntu",
       size = 10
     )
   )
+p
+record_polaroid()
 
 
 # Save --------------------------------------------------------------------
 
 ggsave(p,
   filename = "2025/maps/11_minimal.png",
-  bg = col_palette[5], height = 5.5, width = 6, units = "in"
+  bg = "#0C2431", height = 5, width = 6, units = "in"
 )
-
-
